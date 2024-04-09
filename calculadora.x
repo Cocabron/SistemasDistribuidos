a1ca1c1bb0ca6e6d94cdae76bfb5ed73a0a0a4d7
Cocabron <120059197+Cocabron@users.noreply.github.com>
2024-04-08 21:53:12 -0600

    Add files via upload

---
+++ calculadora.x
@@ -0,0 +1,16 @@
+/* Archivo rand.x */
+
+struct numeros { 
+double a;
+double b;
+};
+
+
+program RAND_PROG{
+ version RAND_VERS{
+double suma(numeros)=1;
+double resta(numeros)=2;
+double multi(numeros)=3;
+double divi(numeros)=4;
+} = 1;
+} = 0x31111111;
---
+++ calculadorafuncion.c
@@ -0,0 +1,18 @@
+/* Archivo random_funcion.c que incluye la definición de las funciones a utilizar */
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "calculadora_local.h"
+
+struct numeros { 
+double a;
+double b;
+};
+
+
+
+double suma(numeros num){return a+b;}
+double resta(numeros num){return a-b;}
+double multi(numeros num){return a*b;}
+double divi(numeros num){return a/b;}
+
